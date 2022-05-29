c94a898a46df324b9b1c86a51ba9ca8e7bde7a33
Cảnh Nguyễn Văn <ngvcanh@192.168.2.9>
2022-05-30 02:47:31 +0700

    Upload code

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # node-log
 Log file path and method called to terminal nodejs
+
+## Usage
+
+This package only support Node environment.
+
+Only import/require from main file of project.
+
+With JavaScript:
+
+```js
+// src/index.js
+require('@kensoni/log');
+
+// More code here
+```
+
+With Typescript:
+
+```ts
+// src/index.ts
+import '@kensoni/log';
+
+// More code here
+```
+
+Call `__log()` function anywhere that you want to show log.
+
+```js
+__log();
+```
+
+Using as `console.log`:
+
+```js
+__log(msg1, msg2, msg3, ...);
+```
+
+The format log message show to terminal
+
+```
+<time>ms [ <000> <line> :: <file path> ] <method> 👉 <message passed>
+```
+
+Demo: 
+
+![Demo Show log](demo.png)
+
+## Note
+
+This package always shows the log whether the environment is production or development.
+
+To disable this feature without removing the code make sure that `process.env.DEBUG_LOG_DISABLED` flag is a truthy.
+
+To change length of zero before `line` value. Let set `process.env.DEBUG_LOG_MIN_COLUMNS` (default: `6`). If value less than 6, it will use default value.
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import 'colors';
+
+declare var global: typeof globalThis & {
+  __log(...msg: any[]): void;
+}
+
+/**
+ * Show log file and method called this function in the terminal. Only support node environment.
+ * @param ...msg any[]
+ * @returns void
+ */
+function __log(...msg: any[]){
+  if (process.env.DEBUG_LOG_DISABLED) return;
+  if (typeof window !== 'undefined' && typeof window.document !== undefined) return;
+
+  const start = Date.now();
+
+  try{
+    throw new Error('Error from __log, skip it if this show wrong');
+  }
+  catch(err){
+    const stack = (err as Error).stack;
+    if (!stack) return;
+
+    let logRow = stack.split("\n")[2];
+    if (!logRow) return;
+
+    logRow = logRow.replace(/^.+at /, '').replace(')', '');
+    const arrInfo = logRow.split('(');
+
+    let method = arrInfo[0];
+    let position = arrInfo[0].split(':');
+
+    if (arrInfo.length === 1){
+      method = '<anonymous>';
+    }
+    else{
+      position = arrInfo[1].split(':');
+    }
+  
+    if (method.match(/^new /)){
+      method = method.replace('new ', '').trim() + ' :: constructor';
+    }
+    else if (method.match(/^Function\./)){
+      method = 'static :: ' + method.replace('Function.', '');
+    }
+    else if (method.match(/^Object.\<anonymous\>/)){
+      method = '';
+    }
+    else{
+      method = method.replace(/\./g, ' :: ');
+    }
+
+    position.pop();
+
+    let line = position.pop() ?? '';
+    let min = parseInt(process.env.DEBUG_LOG_MIN_COLUMNS ?? '');
+
+    if (!min || min < 6) min = 6;
+    if (line.length < min) line = '0'.repeat(min - line.length).white + ' ' + line.yellow;
+
+    const rootPath = process.env.npm_package_json?.replace('package.json', '') ?? '';
+    const fileName = position.join(':').replace(rootPath, '');
+
+    const msgLog = [
+      `${ (Date.now() - start + 'ms').red } ` + 
+      `[ ${ line } :: ${ fileName.yellow } ]` +
+      ` ${ method }`.green,
+    ];
+
+    msg.length && msgLog.push('👉', ...msg);
+    console.log(...msgLog);
+  }
+}
+
+global.__log = __log;
+
+export {}
